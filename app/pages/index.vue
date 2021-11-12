62e9abc93bca1794b2e50faabd1227133e5ae74b
Gu <dlfrnaos19@gmail.com>
2021-11-12 14:07:43 +0900

    c

---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-row>
+    <v-col> <Form /> </v-col>
+    <v-col>
+      <Table />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
